a6a2d4634f5c66ba2c98994411c2ea80648cb85d
ramanahuja1707 <ramanahuja1707@gmail.com>
2014-10-14 21:20:38 +0530

    user dto edited !!!

--- KhanaPakao/src/com/khanapakao/dto/User.java
+++ KhanaPakao/src/com/khanapakao/dto/User.java
@@ -8,25 +8,22 @@ public class User {
 
 	@Id
 	String userMailId;
-	String userPassword;
 	ArrayList<String> userWish;
+
 	public String getUserMailId() {
 		return userMailId;
 	}
+
 	public void setUserMailId(String userMailId) {
 		this.userMailId = userMailId;
 	}
-	public String getUserPassword() {
-		return userPassword;
-	}
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
-	}
+
 	public ArrayList<String> getUserWish() {
 		return userWish;
 	}
+
 	public void setUserWish(ArrayList<String> userWish) {
 		this.userWish = userWish;
 	}
-	
+
 }
